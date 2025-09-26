bec6c0b423433c102038e0513cfd90e6ecd339c7
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-09-26 18:50:00 +0800

    Split `Purchase` into `PurchaseFish` and `PurchaseChum`

--- StoreManager.cs
+++ StoreManager.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Numerics;
 
 namespace ThatOnePVZMinigame;
 public static class StoreManager {
@@ -19,17 +15,9 @@ public static class StoreManager {
 		Money += amount;
 	}
 
-	public static void Purchase<T>(bool v = false, int s = 0) {
-		if (!typeof(T).IsSubclassOf(typeof(Fish))) {
-			Log.Err(() => $"Cannot purchase fish of type {typeof(T).Name} because it does not inherit from Fish.", v, s + 1);
-			return;
-		}
-
-		int cost = 0;
-		cost = typeof(T).Name switch {
-			nameof(Cod) => Cod.Cost,
-			_ => int.MaxValue
-		};
+	public static void PurchaseFish<T>(bool v = false, int s = 0) where T : Fish {
+		Log.Me(() => $"Attempting to purchase a {typeof(T).Name}...", v, s + 1);
+		int cost = Fish.Cost;
 
 
 		if (Money < cost) {
@@ -40,6 +28,23 @@ public static class StoreManager {
 		Money -= cost;
 		Log.Me(() => $"Purchased a {nameof(T)} for {cost}. Remaining money: {Money}.", v, s + 1);
 		WorldManager.SpawnFish<T>(v, s + 1);
+
+		Log.Me(() => "Done!", v, s + 1);
+	}
+
+
+	public static void PurchaseChum<T>(Vector2 startPos, bool v = false, int s = 0) where T : Chum {
+		Log.Me(() => $"Attempting to purchase a {typeof(T).Name}...", v, s + 1);
+		int cost = Chum.Cost;
+
+		if (Money < cost) {
+			Log.Me(() => $"Not enough money to purchase a {nameof(T)}. Needed: {cost}, Available: {Money}.", v, s + 1);
+			return;
+		}
+
+		Money -= cost;
+		Log.Me(() => $"Purchased a {nameof(T)} for {cost}. Remaining money: {Money}.", v, s + 1);
+		WorldManager.SpawnChum<T>(startPos, v, s + 1);
 
 		Log.Me(() => "Done!", v, s + 1);
 	}
