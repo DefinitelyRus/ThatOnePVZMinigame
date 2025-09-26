8fbf654b95da559317571741e1d6498514f896b9
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-09-27 01:34:57 +0800

    Create Poop.cs

---
+++ Poop.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+namespace ThatOnePVZMinigame;
+
+public class Poop : SinkingObject {
+
+	public float HealAmount { get; private set; } = 25f;
+
+	public Poop(
+		Vector2 startPos,
+		bool v = false,
+		int s = 0
+		) : base (
+			startPos: startPos,
+			textureName: "poop",
+			dropRate: 10f,
+			v: v,
+			s: s + 1
+		) {
+		Log.Me(() => $"Created poop.", v, s + 1);
+	}
+}
