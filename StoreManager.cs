b5d5b89230235d5216141c7f262bbf229005cf81
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-09-27 10:43:32 +0800

    Fixed Purchase methods

--- StoreManager.cs
+++ StoreManager.cs
@@ -19,17 +19,24 @@ public static class StoreManager {
 	}
 
 	public static void PurchaseFish<T>(bool v = false, int s = 0) where T : Fish {
-		Log.Me(() => $"Attempting to purchase a {typeof(T).Name}...", v, s + 1);
-		int cost = Fish.Cost;
+		Type type = typeof(T);
 
+		Log.Me(() => $"Attempting to purchase a {type.Name}...", v, s + 1);
+		int cost = type.Name switch {
+			nameof(Cod) => Cod.Cost,
+			nameof(Bass) => Bass.Cost,
+			nameof(JanitorFish) => JanitorFish.Cost,
+			nameof(CarnivoreFish) => CarnivoreFish.Cost,
+			_ => throw new ArgumentException($"Unsupported fish type: {type.Name}", nameof(T)),
+		};
 
 		if (Money < cost) {
-			Log.Me(() => $"Not enough money to purchase a {nameof(T)}. Needed: {cost}, Available: {Money}.", v, s + 1);
+			Log.Me(() => $"Not enough money to purchase a {type.Name}. Needed: {cost}, Available: {Money}.", v, s + 1);
 			return;
 		}
 
 		Money -= cost;
-		Log.Me(() => $"Purchased a {nameof(T)} for {cost}. Remaining money: {Money}.", v, s + 1);
+		Log.Me(() => $"Purchased a {type.Name} for {cost}. Remaining money: {Money}.", v, s + 1);
 		WorldManager.SpawnFish<T>(v, s + 1);
 
 		Log.Me(() => "Done!", v, s + 1);
@@ -37,8 +44,14 @@ public static class StoreManager {
 
 
 	public static void PurchaseChum<T>(Vector2 startPos, bool v = false, int s = 0) where T : Chum {
+		Type type = typeof(T);
+
 		Log.Me(() => $"Attempting to purchase a {typeof(T).Name}...", v, s + 1);
-		int cost = Chum.Cost;
+		int cost = type.Name switch {
+			nameof(SmallChum) => SmallChum.Cost,
+			nameof(LargeChum) => LargeChum.Cost,
+			_ => throw new ArgumentException($"Unsupported chum type: {typeof(T).Name}", nameof(T)),
+		};
 
 		if (Money < cost) {
 			Log.Me(() => $"Not enough money to purchase a {nameof(T)}. Needed: {cost}, Available: {Money}.", v, s + 1);
